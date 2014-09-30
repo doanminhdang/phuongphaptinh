0a6f902c6ea06c166bf91587637487368ac233b5
doanminhdang <minhdang@linux.com>
2014-09-30 10:58:27 +0700

    Source Octave cua SV - cap nhat ngay 2014-09-29

---
+++ thuchanh/baitap_sv/tuan01/thiminhnhut/thiminhnhut-20140914-2.m
@@ -0,0 +1,67 @@
+a=20
+b=5
+c1=a+b
+c2=a-b
+c3=a*b
+c4=a/b
+n=5
+factorial(n)
+n=13
+factorial(n)
+c5=a^b
+c6=b^a
+btext='Kieu van ban'
+who
+whos
+v=[1 2 3 4 5 6]
+v1=[1;2;3;4;5;6]
+v(1)
+v(4)
+v1(1)
+v1(3)
+v
+v1
+v*v1
+v1*v
+v2=v*v1
+v3=v2*v
+v
+length(v)
+v1
+length(v1)
+size(v)
+size(v1)
+v1*v
+size(ans)
+matran=[1 2 3 4; 4 3 2 1; 5 6 7 8; 8 7 6 5]
+size(matran)
+matran_ghep=[matran matran]
+matran_ghep1=[matran matran; matran matran]
+matran
+repmat(matran,3,4)
+repmat(matran,2,1)
+matran
+matran(2,2)
+matran(1,4)
+matran(3,4)
+v=matran(1,1:4)
+v1=matran(2,1:4)
+v2=matran(1:3,3)
+matran_lon=[matran matran; matran matran]
+matran_con=matran_lon(1:3,2:4)
+matran_con=matran_lon(1:2,[1,3])
+matran_con=matran_lon(1:5,[1,3])
+matran_donvi3=eye(3)
+matran_dvi8=eye(8)
+matran_rand=rand(4,5)
+matran_magic=magic(5)
+sum(matran_magic(1:3,2))
+v
+sum(v)
+v
+v'
+matran
+matran'
+matran1=matran+matran
+matran2=matran-matran
+matran3=matran*matran
---
+++ thuchanh/baitap_sv/tuan03/thiminhnhut/thiminhnhut-20140923.m
@@ -0,0 +1,6 @@
+a=2
+b=5
+eps=10^(-6)
+test_nargin(a,b,eps)
+test_nargin(a,b)
+test_nargin(a)
